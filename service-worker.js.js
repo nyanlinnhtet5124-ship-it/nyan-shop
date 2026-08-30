611dd5039672014ce5587b3edc5f6535f49a0207
nyanlinnhtet5124-ship-it <nyanlinnhtet5124@gmail.com>
2026-08-30 08:59:35 +0630

    Update service-worker.js.js

--- service-worker.js.js
+++ service-worker.js.js
@@ -1,21 +1,34 @@
-const CACHE = "nyan-shop-v1";
+const CACHE="nyan-shop-v1";
 
-const FILES = [
+const FILES=[
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./service-worker.js"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install",event=>{
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
+    caches.open(CACHE).then(cache=>cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch",event=>{
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
+    caches.match(event.request).then(cached=>{
+      return cached || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys
+          .filter(key=>key!==CACHE)
+          .map(key=>caches.delete(key))
+      )
     )
   );
 });
